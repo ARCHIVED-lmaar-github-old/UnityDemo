b9ec470d1051ca7c2dae4de53eac35442bd0c4ab
Laurence Maar <56456030+laurencemaar@users.noreply.github.com>
2019-11-25 18:14:41 -0800

    Add files via upload

---
+++ ObjectFloat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFloat : MonoBehaviour
+{
+    [Tooltip("Frequency at which the item will move up and down")]
+    public float verticalBobFrequency = 1f;
+    [Tooltip("Distance the item will move up and down")]
+    public float bobbingAmount = 1f;
+    [Tooltip("Rotation angle per second")]
+    public float rotatingSpeed = 360f;
+
+    Vector3 m_StartPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Remember start position for animation
+        m_StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Handle bobbing
+        float bobbingAnimationPhase = ((Mathf.Sin(Time.time * verticalBobFrequency) * 0.5f) + 0.5f) * bobbingAmount;
+        transform.position = m_StartPosition + Vector3.up * bobbingAnimationPhase;
+
+        // Handle rotating
+        transform.Rotate(Vector3.up, rotatingSpeed * Time.deltaTime, Space.Self);
+    }
+}
